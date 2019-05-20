38aa490ad13bbcd1b482ac503d64b1cac3731f65
U-AD\iaad5777 <iaad5777@CIRES-EL-DW-004.ad.colorado.edu>
2019-05-20 13:41:42 -0600

    implemented evi (too dark)

--- ndvi.py
+++ ndvi.py
@@ -1,5 +1,6 @@
 # purpose: read an rgb and nir image, combine and export into an ndvi
 # (and possibly other xVI) images
+# for this to work on a personal computer, change the path variable to whatever it is on your machine
 
 import numpy as np
 # import pandas as pd # for dataframe
@@ -36,6 +37,10 @@ def crop_smallest(im1, im2):
     return im1, im2
 
 
+def reject_outliers(data, m = 2.):
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
+
+
 def normalize_255(im1, im_color):
     # input: im1 f64 numpy array with some sort of vegetation index info
     # input: im_color numpy array holding original rgb values of the xVI image
@@ -44,24 +49,13 @@ def normalize_255(im1, im_color):
     im_norm[im_norm < 0] = 0  # removes negative values (water, soil)
     im_norm = np.uint8(im_norm)  # round off and make uint8 (imageio likes it)
 
-    im_norm[:, :, 1] = im_norm[:, :, 0]  # add plant data on green channel
     im_norm[:, :, 0] = im_color[:, :, 0]  # add visual red
-    im_norm[:, :, 2] = im_color[:, :, 2]  # add visual blue (uness but looks better)
+    # im_norm[:, :, 2] = im_color[:, :, 2]  # add visual blue (uness but looks better)
 
     return im_norm
 
-
-def IVRL_get_image(full_in_path):
-    #categories = ["country", "field", "forest", "indoor", "mountain", "oldbuilding", "street", "urban", "water"]
-    # Image and Visual Representation Lab
-    base = imageio.imread(full_in_path)
-    im1 = base[0:1,2:3,:]
-    im2 = base[0:1,2:3,:]
-    
-    return im1, im2
-
 # define image paths
-prefix = "landscape"
+prefix = "tree"
 path = "C:/Users/iaad5777/Documents/git/EarthLab/photos/"
 rgb_name = prefix + "_rgb.jpg"
 nir_name = prefix + "_nir.jpg"
@@ -75,28 +69,43 @@ rgb, nir = crop_smallest(rgb, nir)
 x_length = np.shape(rgb)[0]
 y_length = np.shape(rgb)[1]
 
-# define data for later use
+# define data for later use (there must be a better way to declare these)
 red = np.zeros((x_length, y_length, 3))
+blue = np.zeros((x_length, y_length, 3))
 ndvi = np.zeros((x_length, y_length, 3))
 savi = np.zeros((x_length, y_length, 3))
-L = 0.5  # constant for SAVI
+evi = np.zeros((x_length, y_length, 3))
+L_savi = 0.5  # constant for SAVI
+L_evi = 1
+C1_evi = 6 #6
+C2_evi = 3 #7.5
+G = 2.5
 
 # calculate vegitation indexes
 # NDVI: Normalized Difference Veg Idx
 # SAVI: Soil Adjusted Veg Idx
 red[:, :, 0] = rgb[:, :, 0]  # extract just red channel
+blue[:,:,0] = rgb[:,:,2]
 
-ndvi[:, :, 0] = (nir[:, :, 0] - red[:, :, 0]) / (red[:, :, 0] + nir[:, :, 0])
-savi[:, :, 0] = ((1 + L) * (nir[:, :, 0] - red[:, :, 0])) / (nir[:, :, 0] + red[:, :, 0] + L)
+# apply calculation result to green channel
+ndvi[:, :, 1] = (nir[:, :, 0] - red[:, :, 0]) / (red[:, :, 0] + nir[:, :, 0])
+savi[:, :, 1] = ((1 + L_savi) * (nir[:, :, 0] - red[:, :, 0])) / (nir[:, :, 0] + red[:, :, 0] + L_savi)
+# evi[:,:,1] = (nir[:,:,0] - red[:,:,0]) / (nir[:,:,0] + C1_evi*red[:,:,0] - C2_evi*blue[:,:,0] + L_evi)
+numer = (nir[:,:,0] - red[:,:,0])
+denom = (nir[:,:,0] + C1_evi*red[:,:,0] - C2_evi*blue[:,:,0] + L_evi)
+evi[:,:,1] = G * np.divide(numer, denom, out=np.zeros_like(numer), where=denom!=0)
 
 # normalize values for better readability
 ndvi_norm = normalize_255(ndvi, rgb)
 savi_norm = normalize_255(savi, rgb)
+evi_norm = normalize_255(evi, rgb)
 
 if np.array_equal(ndvi_norm, savi_norm):
-    print("ndvi and savi are somehow the same")
+    print("something when wrong with savi calculation")
 
 # write image
 imageio.imwrite(path + prefix + "_ndvi.jpg", ndvi_norm[:, :])
+imageio.imwrite(path + prefix + "_evi.jpg", evi_norm[:,:])
 # imageio.imwrite(path + prefix + "_savi.jpg", savi_norm[:, :])
 # savi ends up having the same values for some reason
+print("done")
