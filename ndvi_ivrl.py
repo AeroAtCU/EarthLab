4f8c112a8abfce58325fe96be0b3bde0eeca8081
U-AD\iaad5777 <iaad5777@CIRES-EL-DW-004.ad.colorado.edu>
2019-05-22 13:05:53 -0600

    finalized, commented

--- ndvi_ivrl.py
+++ ndvi_ivrl.py
@@ -1,36 +1,49 @@
-# for creating ndvi images from the "Image and Visual Represtatino Lab" IVRL
+# for creating ndvi images from the "Image and Visual Represtatino Lab" IVRL dataset. Download:
 # https://ivrl.epfl.ch/research-2/research-downloads/supplementary_material-cvpr11-index-html/
-# for this to work on a personal computer, change the in_path variable to whatever it is on your machine
+# Ian Adler
+# 2019
 
 import numpy as np
 import imageio
 import os
 import sys
+# from pathlib import Path # better way to handle files, currently unecessary
 
 
-def normalize_255(im1, im_color):
-    # input: im1 f64 numpy array with some sort of vegetation index info
-    # input: im_color numpy array holding original rgb values of the xVI image
-    # purpose: take an xVI image and make it human readable and jpg exportable
-    im_norm = im1
-    im_norm[im_norm > 10] = 0  # remove large outliers (works b/c functions should always normalize)
-    im_norm[im_norm > 1] = 1 # remove outliers (AFAIK, veg idx should only go -1:1)
-    im_norm[im_norm < 0] = 0  # removes negative values (water, soil)
-    im_norm = (im_norm / np.max(im1)) * 255  # gives greatest contrast w/out losing data
-    im_norm = np.uint8(im_norm)  # round off and make uint8 (imageio likes it)
+def normalize_vegidx(vegidx_im, rgb):
+    # input: vegidx_im f64 numpy array containing vegetation index info
+    # input: rgb numpy array holding original color values of the image
+    # purpose: take vegitation index calculated data and make it human readable and exportable
 
-    im_norm[:, :, 0] = im_color[:, :, 0]  # add visual red
-    # im_norm[:, :, 2] = im_color[:, :, 2]  # add visual blue (uness but looks better)
+    # remove outliers (vegidx should always go between -1:1)
+    vegidx_im[vegidx_im > 5] = 0
+    vegidx_im[vegidx_im > 1] = 1
+    vegidx_im[vegidx_im < 0] = 0
 
-    return im_norm
+    # normalize the values to work best with images
+    vegidx_im *= 255 / np.max(vegidx_im) # equivalent to vegidx_im = 255 * (vegidx_im / np.max(vegidx_im)), but probably faster
+    vegidx_im = np.uint8(vegidx_im)  # make uint8 (round and change datatype. imageio prefers uint8)
+
+    # add visual bands
+    vegidx_im[:, :, 0] = rgb[:, :, 0] # rgb[x,y,z] {z=(0,1,2) -> (r,g,b)}
+
+    return vegidx_im
 
 
 def check_shape(nir, rgb):
+    # input: nir x*y numpy array
+    # input: rgb x*y*3 numpy array
+    # purpose: check if x and y dims are the same, exit if not (should always be the same)
     if np.shape(rgb)[0] != np.shape(nir)[0] or np.shape(rgb)[1] != np.shape(nir)[1]:
-        print("Dimension Mismatch, exiting")
+        print("Dimension mismatch, exiting.")
         sys.exit(1)
 
+
 def calc_evi(nir, rgb):
+    # input: nir x*y numpy array representing a picture
+    # input: rgb x*y*3 numpy array representing a picture
+    # purpose: calculate the enhanced vegitation index
+
     check_shape(nir, rgb) # make sure sizes are equal
     xleng = np.shape(rgb)[0]
     yleng = np.shape(rgb)[1]
@@ -40,78 +53,88 @@ def calc_evi(nir, rgb):
     # create base zero arrays
     red = np.zeros((xleng, yleng, 3))
     blue = np.zeros((xleng, yleng, 3))
-    evi = np.zeros((xleng, np.shape(rgb)[1], 3))
-    numer = np.zeros((xleng, np.shape(rgb)[1], 3))
-    denom = np.zeros((xleng, np.shape(rgb)[1], 3))
+    evi = np.zeros((xleng, yleng, 3))
+    numer = np.zeros((xleng, yleng, 3))
+    denom = np.zeros((xleng, yleng, 3))
     
-    # doesn't like it when using rgb, so extract one color per channel
+    # doesn't like it when using rgb, so extract one color per channel needed
     red[:,:,0] = rgb[:,:,0]
     blue[:,:,2] = rgb[:,:,2]
     
-    # run calculation. np.divide(options) prevents divion by zero (sets any/0 to 0)
+    # run calculation. np.divide() prevents divion by zero (sets any/0 to 0). set result to green channel.
     numer = nir - red[:,:,0]
     denom = (nir + C1*red[:,:,0] - C2*blue[:,:,2] + L)
     evi[:,:,1] = G * (np.divide(numer, denom, out=np.zeros_like(numer), where=denom!=0))
     
-    evi_norm = normalize_255(evi, rgb)
+    # normalize values for picture
+    evi_norm = normalize_vegidx(evi, rgb)
     
     return evi_norm, evi
 
 def calc_ndvi(nir, rgb):
+    # input: nir x*y numpy array representing a picture
+    # input: rgb x*y*3 numpy array representing a picture
+    # purpose: calculate the normalized difference vegitation index
+    # notes: nir is a grid of NIR reflected values. rgb[:,:,0] is a grid of red reflected values.
+    
     check_shape(nir, rgb) # make sure sizes are equal
+    xleng = np.shape(rgb)[0]
+    yleng = np.shape(rgb)[1]
     
     # create base zero arrays
-    red = np.zeros((np.shape(rgb)[0], np.shape(rgb)[1], 3))
-    ndvi = np.zeros((np.shape(rgb)[0], np.shape(rgb)[1], 3))
+    red = np.zeros((xleng, yleng, 3))
+    ndvi = np.zeros((xleng, yleng, 3))
     
-    red[:,:,0] = rgb[:,:,1] # extract red
-    # nir is a x by y grid of NIR reflected values. rgb[:,:,0] is a x by y grid of red reflected values.
-    ndvi[:, :,1] = (nir - red[:, :, 0]) / (red[:, :, 0] + nir) # run calculation and set to green channel
+    # doesn't like it when using rgb, so extract one color per channel needed
+    red[:,:,0] = rgb[:,:,1]
+    ndvi[:, :,1] = (nir - red[:, :, 0]) / (red[:, :, 0] + nir) # set result to green channel
     
-    ndvi_norm = normalize_255(ndvi, rgb) # norm values to make changes easier to see
+    # normalize values for picture
+    ndvi_norm = normalize_vegidx(ndvi, rgb)
     
     return ndvi_norm, ndvi
     
-
-# dir_path = "C:/Users/iaad5777/Documents/git/EarthLab/nirscene1/country/" # uncomment this and change it to exact path if os.path.<etc> doesn't work
-dir_path = os.path.dirname(os.path.abspath(__file__)) + "/nirscene1/forest/"
+# define inputs path and type (currently only tested on .tiffs from above dataset)
+script_path = os.path.dirname(os.path.abspath(__file__)) # get current (script) directory
+ivrl_folder = os.path.join("nirscene1", "country") # join with the dataset
+read_path = os.path.join(script_path, ivrl_folder, "")
 ext_nir = "_nir.tiff"
 ext_rgb = "_rgb.tiff"
+write_path = read_path # provides options
+out_ext = ".tiff" # exporting to tiff is faster than jpg
 
-write_path = dir_path # provides options
-out_ext = ".tiff" # tiff faster than jpg
+verbose = True # describe each file or just symbols
+# ? = FileNotFoundError; x = file cannot be used; . = new picture being converted 
 
-verbose = False # Display every image vs only errors
+print("beginning loop inside " + read_path) if verbose else print("start")
 
-print("beginning loop inside " + dir_path) if verbose else print("start")
-for filename in os.listdir(dir_path): # for every file in dir_path (NOT IN ORDER)
+for filename in os.listdir(read_path): # for every file in read_path (NOT IN ORDER)
     try:
         if filename.endswith(ext_nir): # if it ends with a certain ext (only want one bc two images being evald)
-            # ternary operator stype. also, print("x", end="") prints without newline
-            #print(filename + " being evaluated") if verbose else print(".", end='')
-            print(filename + " being evaluated") if verbose else print(".", end = "")
+            # ternary operator style. also, print("x", end="") prints without newline
+            print(filename + " being evaluated") if verbose else print(".", end="")
             
             # import images (split splits after _, [0] takes first element [the actual filename])
-            rgb = imageio.imread(dir_path + filename.split("_")[0] + ext_rgb)
-            nir = imageio.imread(dir_path + filename.split("_")[0] + ext_nir)
+            rgb = imageio.imread(read_path + filename.split("_")[0] + ext_rgb)
+            nir = imageio.imread(read_path + filename.split("_")[0] + ext_nir)
                         
             # calculate indices
-            ndvi_norm, ndvi = calc_ndvi(nir, rgb)
-            evi_norm, evi = calc_evi(nir, rgb)
+            ndvi_norm, nop = calc_ndvi(nir, rgb)
+            evi_norm, nop = calc_evi(nir, rgb)
             
             # export images
             imageio.imwrite(write_path + filename.split("_")[0] + "_ndvi" + out_ext, ndvi_norm[:,:])
             imageio.imwrite(write_path + filename.split("_")[0] + "_evi" + out_ext, evi_norm[:,:])
-        elif filename.endswith(ext_rgb): 
-            pass # nec bc only want below when filename != _rgb.tiff || _nir.tiff
+        elif filename.endswith(ext_rgb) or filename.endswith("vi.tiff"): # not best way to do this
+            pass # necessary bc of the file and loop structure
         else:
-            print(filename + " is not a file I can use") if verbose else print("", end='') # to provide options
+            print(filename + " is not a file I can use") if verbose else print("x", end="")
             
     except FileNotFoundError:
-        print(filename + " does not exist")
+        print(filename + " does not exist") if verbose else print("?", end="")
         
     except:
         print("something went wrong, exiting")
         sys.exit(1)
 
-print("done")
+print('\n' + "done")
