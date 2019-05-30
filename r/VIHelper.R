368867df5bbf462bc2d457c879a2660fbea85904
U-AD\iaad5777 <iaad5777@CIRES-EL-DW-004.ad.colorado.edu>
2019-05-30 15:12:04 -0600

    beginnings of r version of NDVI_IVRL

---
+++ r/VIHelper.R
@@ -0,0 +1,16 @@
+calcNDVI <- function(rgb_im, nir_im, verbose = FALSE){
+  # Calculate the normalized difference vegitation index
+  #
+  # Args:
+  #   rgb_im: x*y*3 dataframe (?) holding original color vals
+  #   nir_im: x*y*any dataframe (?) holding near infrared vals
+  #   verbosity: if TRUE, print small updates. else nothing.
+  #
+  # Returns:
+  #   an x*y*3 dataframe (?) of zeros with straight NDVI vals on G channel
+  cat("calcNDVI called")
+}
+
+testPrint <- function(){
+  cat("asdf")
+}
---
+++ r/create_ndvi.r
@@ -0,0 +1,6 @@
+# get 2 images, convert to ndvi, save output image. will be a function
+
+# this is not a good way to do this but I don't want to install here() rn
+source("C:/Users/iaad5777/Documents/git/EarthLab/r/VIHelper.R")
+
+testPrint()
