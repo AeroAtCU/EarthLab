b7e514297c9bdeb4b3418a4fcbad21e06a602a55
U-AD\iaad5777 <iaad5777@CIRES-EL-DW-004.ad.colorado.edu>
2019-06-13 12:28:18 -0600

    testing magick

--- r/create_ndvi.R
+++ r/create_ndvi.R
@@ -1,4 +1,6 @@
 # import 2 images given filepaths/ names, convert to ndvi, save output image. will be a function eventaully
+# https://www.agisoft.com/pdf/PS_1.3%20-Tutorial%20(BL)%20-%20Orthophoto,%20DEM%20(GCPs).pdf
+# save.as
 
 # install.packages("package_name")
 # for magick, you might have to install imagemagick or it's extras first. Xubuntu wanted:
@@ -14,6 +16,15 @@ nirscene1_path = file.path(this_parent,"nirscene1") # hardcoded but should alway
 out_path <- "C:\\Users\\iaad5777\\Documents\\git\\EarthLab\\r_tmpout" # bad
 
 rgb_path = file.path(nirscene1_path,"country","0000_rgb.tiff")
+nir_path = file.path(nirscene1_path,"country","0000_nir.tiff")
 
-tmp_input <- magick::image_read(rgb_path)
-magick::image_write(tmp_input, path=out_path, format = "png") #cannot write the image
+rgb <- magick::image_read(rgb_path)
+nir <- magick::image_read(nir_path)
+red <- magick::image_channel(tmp, "red")
+
+red_nir <- c(nir, red)
+red_nir_sub <- magick::image_flatten(red_nir,"Minus")
+
+magick::image_browse(red_nir_sub)
+magick::image_write(tmp_input, path=out_path, format = "png") #cannot write the image on student computer, not admin
+#
