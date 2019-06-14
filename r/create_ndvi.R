94a4d902cdc1e421982ce23afa6e3448f76e9a9f
AeroAtCU <ian.adler@colorado.edu>
2019-06-14 12:45:24 -0600

    minor improvements

--- r/create_ndvi.R
+++ r/create_ndvi.R
@@ -13,18 +13,20 @@ library("magick")
 this_wd = getwd()
 this_parent = dirname(this_wd)
 nirscene1_path = file.path(this_parent,"nirscene1") # hardcoded but should always be the same if using correct dataset.
-out_path <- "C:\\Users\\iaad5777\\Documents\\git\\EarthLab\\r_tmpout" # bad
-
+out_path = file.path(this_parent,"r_tmpout")
+  
 rgb_path = file.path(nirscene1_path,"country","0000_rgb.tiff")
 nir_path = file.path(nirscene1_path,"country","0000_nir.tiff")
 
+# grab images and extract color channels (seems to work)
 rgb <- magick::image_read(rgb_path)
 nir <- magick::image_read(nir_path)
-red <- magick::image_channel(tmp, "red")
+red <- magick::image_channel(rgb, "red")
 
-red_nir <- c(nir, red)
-red_nir_sub <- magick::image_flatten(red_nir,"Minus")
+# subtract red from NIR (does not work)
+red_nir <- c(rgb; nir)
+red_nir_sub <- magick::image_flatten(red_nir,operator="Minus")
 
 magick::image_browse(red_nir_sub)
-magick::image_write(tmp_input, path=out_path, format = "png") #cannot write the image on student computer, not admin
+magick::image_write(red_nir_sub, path=out_path, format = "png") #cannot write the image on student computer, not admin
 #
