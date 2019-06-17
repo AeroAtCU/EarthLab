04e22a8b8eb909e0f7faedefa731143c126cd458
U-AD\iaad5777 <iaad5777@CIRES-EL-DW-004.ad.colorado.edu>
2019-06-17 15:26:16 -0600

    ndvi normalized

--- r/create_ndvi.R
+++ r/create_ndvi.R
@@ -3,7 +3,8 @@
 
 # for magick, you might have to install imagemagick or it's extras first. Xubuntu wanted:
 # sudo apt install libmagick++-dev
-rm(list=ls())
+# ":s/^/# /" will crash rstudio
+rm(list=ls()) # clear all variables. Apparently bad practice but seemingly necessary
 
 library("magick")
 library("raster")
@@ -11,18 +12,27 @@ library("rgeos")
 library("rgdal")
 library("RColorBrewer")
 options(stringsAsFactors = FALSE) # want strings to be string s
+#source("some_function.R")
 
-this_wd <- getwd()
+# Define filepaths and strings (some will need to be looped)
+# this_wd <- getwd()
+this_wd <- "C:/Users/iaad5777/Documents/git/EarthLab/r"
 images_path <- file.path(dirname(this_wd),"nirscene1") # assuming folder is in parent
+img_num <- "0001" # eventually, how to convert int to 0 padded string?
+subfolder <- "forest"
 
-img_num <- "0000" # eventually, how to convert int to 0 padded string?
-subfolder <- "country"
-
+# get normal and IR images
 rgb <- stack(file.path(images_path, subfolder, (paste(img_num,"_rgb.tiff",sep=""))))
+out <- rgb
 nir <- stack(file.path(images_path, subfolder, (paste(img_num,"_nir.tiff",sep=""))))
 
-ndvi <- (nir[[1]] - rgb[[3]]) / (nir[[1]] + rgb[[3]])
+# calculate the ndvi. Why img[[x]]?
+ndvi <- (nir[[1]] - rgb[[1]]) / (nir[[1]] + rgb[[1]])
 
-plot(ndvi,
+# normalize values so it looks good
+ndvi_norm <- raster::stretch(ndvi, minv = 0, maxv = 255, minq = -1, maxq = 1)
+
+# create output image.
+plot(ndvi_norm,
      main = paste(img_num,"_nir.tiff",sep=""),
      axes = FALSE, box = FALSE)
