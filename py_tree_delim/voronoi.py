cf8da732b6d1b72d503170c4f5dbef24d196df44
AeroAtCU <ian.adler@colorado.edu>
2019-07-26 13:20:19 -0600

    minor changes

--- py_tree_delim/voronoi.py
+++ py_tree_delim/voronoi.py
@@ -1,7 +1,7 @@
-# eventually, take x y tree crown centerpoints and create a thiessen (voronoi) polygon from them for comparison to negative slope map
+# eventually, take x y tree crown centerpoints (pixel locations) and create a thiessen (voronoi) polygon from them for comparison to negative slope map
 import matplotlib.pyplot as plt
 from scipy.spatial  import Voronoi, voronoi_plot_2d
-iport numpy as np
+import numpy as np
 
 points = np.array([[0.5, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2],[2, 0], [2, 1], [2, 2]])
 
