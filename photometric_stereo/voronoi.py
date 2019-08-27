0948686910a904846def2f25633e3020ac99a1ff
AeroAtCU <ian.adler@colorado.edu>
2019-08-27 12:57:03 -0600

    appending to end of 2d array not working

--- photometric_stereo/voronoi.py
+++ photometric_stereo/voronoi.py
@@ -3,8 +3,22 @@
 import matplotlib.pyplot as plt
 from scipy.spatial  import Voronoi, voronoi_plot_2d
 import numpy as np
+import random
 
-points = np.array([[0.5, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2],[2, 0], [2, 1], [2, 2]])
+
+points = np.array([[0,0]])
+for x in range(10):
+    a = random.randint(1,11)
+    b = random.randint(1,11)
+    print(a,b)
+    tmp = [a,b]
+    np.concatenate((points[0], tmp))
+
+
+points_working = np.array([[0.5, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2],[2, 0], [2, 1], [2, 2]])
+
+print(points)
+print(points_working)
 
 vor = Voronoi(points)
 fig = voronoi_plot_2d(vor)
